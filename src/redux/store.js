528611f3573be1a9f83e29adfe45ba57430ff5d3
Olena457 <olenademianenko42@gmail.com>
2024-06-21 10:19:41 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filtersReducer } from './filtersSlice';
 import { contactsReducer } from './contactsSlice';
+// import storage from 'redux-persist/lib/storage';
 
 export const store = configureStore({
   redusers: {
